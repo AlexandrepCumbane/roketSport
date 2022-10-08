8b77030cff06277d90908b8fa3fc468cdaf64b3f
Alexandre Cumbane <alexandrepcumbane@gmail.com>
2022-10-08 15:03:26 +0200

    import fontes

--- sportspp/Mobile/App.tsx
+++ sportspp/Mobile/App.tsx
@@ -1,7 +1,23 @@
  import {StatusBar} from 'react-native'
  import { Background } from './src/components/Background';
+ import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold, 
+  Inter_900Black
+} from '@expo-google-fonts/inter'
+
 
 export default function App() {
+
+  useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold, 
+    Inter_900Black
+  });
+  
   return (
     <Background >
       <StatusBar
