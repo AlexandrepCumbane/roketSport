928624033edbdc7aa63252f48ee0b7a73e242189
Alexandre Cumbane <alexandrepcumbane@gmail.com>
2022-10-11 00:27:33 +0200

    update routes

--- sportspp/server/src/server.ts
+++ sportspp/server/src/server.ts
@@ -21,7 +21,7 @@ app.post('/ads',(req, res)=>{
     return res.status(201).json([])
 })
 
-app.get('/ads', (req, res) => {
+app.get('/games/:id/ads', (req, res) => {
     return res.json([
         {id: '1', name: 'Alex'},
         {id: '2', name: 'Alex1'},
@@ -29,6 +29,10 @@ app.get('/ads', (req, res) => {
         {id: '4', name: 'Alexxss'},
         {id: '4', name: 'Alexxss'},
     ])
+})
+
+app.get('/ads/:id/discord ', (req, res) => {
+    return res.json([])
 })
 
 app.listen(3001)
