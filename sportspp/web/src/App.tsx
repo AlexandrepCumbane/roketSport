b9079f12bc2d1448e2d1fff3276ab985038f16b0
Alexandre Cumbane <alexandrepcumbane@gmail.com>
2022-10-14 00:28:17 +0200

    add components in the app

--- sportspp/web/src/App.tsx
+++ sportspp/web/src/App.tsx
@@ -1,6 +1,8 @@
 import './styles/main.css';
 import logoImg from './assets/logo.svg';
 import {MagnifyingGlassPlus} from 'phosphor-react'
+import { GameBanner } from './components/GameBanner';
+import { CreateAdBanner } from './components/CreateAdBanner';
 function App() {
 
   return (
@@ -12,62 +14,11 @@ function App() {
 
       {/* list of games */}
       <div className='grid grid-cols-6 gap-6 mt-16' >
-          <a href="" className="relative rounded-lg">
-            <img src= "/game-1.png"/> 
-            <div className='w-full pt-16 pb-4 px-4 absolute bottom-0 left-0 right-0'>
-              <strong className='font-bold text-white block'>League of  Legends</strong>
-              <span className='text-zinc-300 text-sm block'>4 anuncios</span>
-            </div>
-          </a>
-          <a href="" className="relative rounded-lg">
-            <img src= "/game-2.png"/> 
-            <div className='w-full pt-16 pb-4 px-4 absolute bottom-0 left-0 right-0'>
-              <strong className='font-bold text-white block'>League of  Legends</strong>
-              <span className='text-zinc-300 text-sm block'>4 anuncios</span>
-            </div>
-          </a>
-          <a href="" className="relative rounded-lg">
-            <img src= "/game-3.png"/> 
-            <div className='w-full pt-16 pb-4 px-4 absolute bottom-0 left-0 right-0'>
-              <strong className='font-bold text-white block'>League of  Legends</strong>
-              <span className='text-zinc-300 text-sm block'>4 anuncios</span>
-            </div>
-          </a>
-          <a href="" className="relative rounded-lg">
-            <img src= "/game-4.png"/> 
-            <div className='w-full pt-16 pb-4 px-4 absolute bottom-0 left-0 right-0'>
-              <strong className='font-bold text-white block'>League of  Legends</strong>
-              <span className='text-zinc-300 text-sm block'>4 anuncios</span>
-            </div>
-          </a>
-          <a href="" className="relative rounded-lg">
-            <img src= "/game-5.png"/> 
-            <div className='w-full pt-16 pb-4 px-4 absolute bottom-0 left-0 right-0'>
-              <strong className='font-bold text-white block'>League of  Legends</strong>
-              <span className='text-zinc-300 text-sm block'>4 anuncios</span>
-            </div>
-          </a>
-          <a href="" className="relative rounded-lg">
-            <img src= "/game-6.png"/> 
-            <div className='w-full pt-16 pb-4 px-4 absolute bottom-0 left-0 right-0'>
-              <strong className='font-bold text-white block'>League of  Legends</strong>
-              <span className='text-zinc-300 text-sm block'>4 anuncios</span>
-            </div>
-          </a>
-      </div>
+          <GameBanner bannerUrl="/game-6.png" title="League of  Legends" adsCount={4}/>
 
-      <div className='pt-1 bg-nlw-gradient  self-stretch rounded-lg mt-8 m-16  overflow-hidden'>
-          <div className='bg-[#2A2634]  px-8 py-6 flex justify-between items-center'>
-            <div>
-              <strong className=' text-2xl text-white  font-black block'>Nao encontrou o seu duo</strong>
-              <span className='text-zinc-400 block'>Publique Um anuncio para encontrar novos players</span> 
-            </div>
-            <button className='px-3 py-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3 '>
-              <MagnifyingGlassPlus size='24'/>
-              publicar anuncio
-            </button>
-          </div>
       </div>
+      <CreateAdBanner/>
+
     </div>
   )
 }
