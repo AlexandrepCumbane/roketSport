0a7f0a50cf2f4f668432b81274c3b11589ac9ac2
Alexandre Cumbane <alexandrepcumbane@gmail.com>
2022-10-13 18:24:00 +0200

    add create fun

--- sportspp/server/src/server.ts
+++ sportspp/server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import {PrismaClient} from '@prisma/client'
 
 const app = express();
+app.use(express.json()) 
 
 // connect to prisma
 const prisma = new PrismaClient({
@@ -26,22 +27,68 @@ app.get('/games',async (req, res)=>{
 })
 
 // create
-app.post('/ads',(req, res)=>{
-    return res.status(201).json([])
+app.post('/games/:id/ads', async(req, res)=>{
+
+    const gameId = req.params.id
+    const body = req.body;
+
+    const ad = await prisma.ad.create({
+        data: {
+            gameId,
+            name:body.name,
+            weekDay:body.weekDay,
+            useVoiceChannel:body.useVoiceChannel,
+            yearsPlaying:body.yearsPlaying,
+            hourStart:body.hourStart,
+            hourEnd:body.hourEnd,
+            discord:body.discord, 
+        }
+    })
+
+    return res.status(201).json(body)
 })
 
 app.get('/games/:id/ads', async (req, res) => {
     const gameId = req.params.id;
     const ads = await prisma.ad.findMany({
+        select:{
+            id:true,
+            name:true,
+            weekDay:true,
+            useVoiceChannel:true,
+            yearsPlaying:true,
+            hourStart:true,
+            hourEnd:true,
+        },
         where: {
              gameId,
+        },
+        orderBy: {
+            createdAt:'desc'
         }
     })
-    return res.json(ads)
+    return res.json(ads.map(ad =>{
+        return {
+            ...ad,
+            weekDay:ad.weekDay.split(','),
+        }
+    }))
 })
 
-app.get('/ads/:id/discord ', (req, res) => {
-    return res.json([])
+app.get('/ads/:id/discord  ', async (req, res) => {
+    const adId = req.params.id;
+    const ad = await prisma.ad.findUniqueOrThrow  ({
+        select:{
+            discord:true,
+        },
+        where: {
+            id:adId,
+        }
+    })
+
+    return res.json({
+        discord:ad.discord
+    })
 })
 
 app.listen(3001)
