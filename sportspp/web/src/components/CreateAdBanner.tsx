221ca7c09c707ded2308623025d5e8ca27271540
Alexandre Cumbane <alexandrepcumbane@gmail.com>
2022-10-14 00:27:09 +0200

    createAdBanner component

---
+++ sportspp/web/src/components/CreateAdBanner.tsx
@@ -0,0 +1,18 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
+
+export function CreateAdBanner(){
+   return(
+        <div className='pt-1 bg-nlw-gradient  self-stretch rounded-lg mt-8 m-16  overflow-hidden'>
+            <div className='bg-[#2A2634]  px-8 py-6 flex justify-between items-center'>
+            <div>
+                <strong className=' text-2xl text-white  font-black block'>Nao encontrou o seu duo</strong>
+                <span className='text-zinc-400 block'>Publique Um anuncio para encontrar novos players</span> 
+            </div>
+            <button className='px-3 py-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3 '>
+                <MagnifyingGlassPlus size='24'/>
+                publicar anuncio
+            </button>
+            </div>
+        </div>
+        )
+} 
