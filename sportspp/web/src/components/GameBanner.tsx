f34ffceb14e51475f2d1b07813b961d3454303f3
Alexandre Cumbane <alexandrepcumbane@gmail.com>
2022-10-14 16:55:21 +0200

    overflow-hidden components

--- sportspp/web/src/components/GameBanner.tsx
+++ sportspp/web/src/components/GameBanner.tsx
@@ -6,7 +6,7 @@
  
  export function GameBanner(props: GameBannerProps){
     return (
-        <a href="" className="relative rounded-lg">
+        <a href="" className="relative rounded-lg overflow-hidden">
             <img src= {props.bannerUrl}  /> 
                 <div className='w-full pt-16 pb-4 px-4 absolute bottom-0 left-0 right-0'>
                 <strong className='font-bold text-white block'>{props.title}</strong>
