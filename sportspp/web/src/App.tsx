5153ee20805abea840967ecbb350ef93fdd8fa47
Alexandre Cumbane <alexandrepcumbane@gmail.com>
2022-10-24 11:14:38 +0200

    orden  input

--- sportspp/web/src/App.tsx
+++ sportspp/web/src/App.tsx
@@ -50,9 +50,11 @@ function App() {
         <CreateAdBanner/>
         <Dialog.Portal>
            <Dialog.Overlay className="bg-black/60 inset-0 fixed"/>
+
            <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/40">
               <Dialog.Title className="text-3xl font-black">Publique Um anuncio</Dialog.Title>
-                <form className="mt-8 ">
+               
+                <form className="mt-8 flex flex-col gap-4">
                   <div className="flex flex-col gap-2">
                     <label htmlFor="game" className="font-semibold">Qual o Game?</label>
                     <Input 
@@ -61,7 +63,7 @@ function App() {
                       />
                   </div>
 
-                  <div>
+                  <div className="flex flex-col gap-2">
                     <label htmlFor="name">seu Nome ( ou nickname)</label>
                     <Input 
                       id="name" 
@@ -69,33 +71,37 @@ function App() {
                       placeholder="Como te chamam os outros"/>
                   </div>
 
-                  <div>
-                    <div>
+                  <div className="grid grid-cols-2 gap-6">
+                    <div className='flex flex-col gap-2'>
                       <label htmlFor="yearsPlaying">Joga ha quantos anos?</label>
                       <Input id="yearsPlaying" type="number" placeholder="Yudo bem ser Zero"/>
                     </div>
-                    <div>
+                    <div className='flex flex-col gap-2'>
                       <label htmlFor="discord">Qual o seu Discord</label>
                       <Input id="discord" type="text" placeholder="usuario#0000"/>
                     </div>
+                  </div>
 
-                    <div>
-                      <div>
-                        <label htmlFor="weekDays">Quando costuma jogar?</label>
-                      </div>
-                      <div>
-                        <label htmlFor="hourStart">Qual o horario do Dia?</label>
-                        <div>
-                          <Input id="hourStart" type="time" placeholder="De"/>
-                          <Input id="hourEnd" type="time" placeholder="De"/>
-                        </div>
+                  <div className='flex gap-6'>
+                    <div className='flex flex-col gap-2'>
+                      <label htmlFor="weekDays">Quando costuma jogar?</label>
+                      <div className='grid grid-cols-4 gap-1'>
                       </div>
                     </div>
-                  </div> 
+                    <div  className='flex flex-col gap-2 flex-1'>
+                      <label htmlFor="hourStart">Qual o horario do Dia?</label>
+                      <div className='grid grid-cols-2 gap-2'>
+                        <Input id="hourStart" type="time" placeholder="De"/>
+                        <Input id="hourEnd" type="time" placeholder="De"/>
+                      </div>
+                    </div>
+                  </div>
+                
                   <div>
                     <input type="checkbox"/>
                     constumo me conectar ao chat de voz
                   </div>
+
                   <footer>
                     <button>Cancelar</button>
                     <button type="submit">
